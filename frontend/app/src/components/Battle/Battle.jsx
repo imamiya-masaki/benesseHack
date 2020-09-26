9c636c11f85cd518f92a9c257445f1703da5b42d
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-09-27 00:19:13 +0900

    Prepare battle page

---
+++ frontend/app/src/components/Battle/Battle.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./Battle.module.css";
+import Navbar from "../Navbar/Navbar";
+
+const Battle = (props) => {
+    return (
+        <div>
+            <Navbar />
+            Battle
+        </div>
+    );
+};
+
+export default Battle;
