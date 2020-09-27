00716048cfa9c8a3b66e0afc71c09700549a13b8
imamiya-masaki <imachan567@gmail.com>
2020-09-27 09:50:58 +0900

    homeへのつなぎ合わせまかせた！！！

--- frontend/app/src/App.js
+++ frontend/app/src/App.js
@@ -4,11 +4,16 @@ import Home from "./components/Home/Home";
 import Stamp from "./components/Stamp/Stamp";
 import Profile from "./components/Profile/Profile";
 import Battle from "./components/Battle/Battle";
+import Login from "./components/Login/Login"
 import './App.css';
-
-function App() {
-  return (
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import reducer from './reducer';
+var global = this
+const store = createStore(reducer)
+const app = (
     <div className="App">
+      <span>{}</span>
       <BrowserRouter>
         <Route path="/" render={() => <Home />} exact />
         <Route path="/stamp" render={() => <Stamp />} exact />
@@ -16,6 +21,20 @@ function App() {
         <Route path="/battle" render={() => <Battle />} exact />
       </BrowserRouter>
     </div>
+  )
+const output = function () {
+  console.log("get", global, store.getState())
+  const user = store.getState().user
+  if (!user.hasOwnProperty("user_id")) {
+    return (<Login/>);
+  }
+  return app
+}
+function App() {
+  return (
+    <Provider store={store}>
+    {output()}
+    </Provider>
   );
 }
 
---
+++ frontend/app/src/components/Login/Login.jsx
@@ -0,0 +1,135 @@
+import React, { useState } from "react";
+import styles from "./Login.module.css";
+import Navbar from "../Navbar/Navbar";
+import axios from "axios";
+import { createStore } from 'redux';
+import reducer from './../../reducer';
+const store = createStore(reducer)
+const Login = (props) => {
+    // var status = "login"
+    var [status, setState] = useState("login")
+    var [loginId, setLoginId] = useState("test1")
+    var [passId, setpassId] = useState("pass1")
+    var [beneseId, setBeneseId] = useState("")
+    var switchng = function () {
+      console.log("status", status)
+      if (status=="login") {
+        // status = "register"
+        setState("register")
+      } else {
+        // status = "login"
+        setState("login")
+      }
+    }
+    const getApi = function () {
+      console.log("check", status)
+      if (status=="register") {
+        axios.post("/api/regist", {
+          login_id: loginId,
+          login_pass: passId,
+          benese_id: ""
+        })
+      }
+      axios.post("http://localhost:8080/api/login", {
+        login_id: loginId,
+          login_pass: passId,
+          benese_id: ""
+      })
+      .then(res => {
+        console.log("check", res)
+        if (res.data.Login_pass.length > 0) {
+         
+        }
+      })
+    }
+    const onLogin = function (get) {
+      // console.log("get", get)
+      setLoginId(get.target.value)
+    }
+    const onPass = function (get) {
+      // console.log("get", get)
+      setpassId(get.target.value)
+    }
+    const onBenesse = function (get) {
+      // console.log("get", get)
+      setBeneseId(get.target.value)
+    }
+    var login = (
+    <div className={styles.container}>
+      <div className="ui middle aligned center aligned grid">
+          <div className="column">
+              <h2 className="ui teal image header">
+                  {status}
+              </h2>
+              <form className="ui large form">
+              <div className="ui stacked segment">
+                  <div className="field">
+                  <div className="ui left icon input">
+                      <i className="user icon"></i>
+                      <input type="text" name="email" value={loginId} onChange={onLogin} placeholder="E-mail address"/>
+                  </div>
+                  </div>
+                  <div className="field">
+                  <div className="ui left icon input">
+                      <i className="lock icon"></i>
+                      <input type="password" name="password"  value={passId} onChange={onPass} placeholder="Password"/>
+                  </div>
+                  </div>
+                  <div className="ui fluid large teal submit button" onClick={getApi}>{status}</div>
+              </div>
+
+              <div className="ui error message"></div>
+              </form>
+              <div className="ui message" onClick={switchng}><a>registet?</a></div>
+          </div>
+      </div>
+    </div>
+    );
+    var regist = (
+      <div className={styles.container}>
+      <div className="ui middle aligned center aligned grid">
+          <div className="column">
+              <h2 className="ui teal image header">
+              {status}
+              </h2>
+              <form className="ui large form">
+              <div className="ui stacked segment">
+                  <div className="field">
+                  <div className="ui left icon input">
+                      <i className="user icon"></i>
+                      <input type="text" name="email" placeholder="E-mail address"/>
+                  </div>
+                  </div>
+                  <div className="field">
+                  <div className="ui left icon input">
+                      <i className="lock icon"></i>
+                      <input type="password" name="password" placeholder="Password"/>
+                  </div>
+                  </div>
+                  <div className="ui fluid large teal submit button">{status}</div>
+              </div>
+
+              <div className="ui error message"></div>
+              </form>
+              <div className="ui message" onClick={switchng}><a>login?</a></div>
+          </div>
+      </div>
+    </div>
+    );
+    var output = function () {
+      console.log("check", status, loginId)
+        if (status=="login") {
+          return login
+        } else {
+          return regist
+        }
+    }
+
+    return (
+      <div className={styles.container}>
+          {output()}
+      </div>
+  );
+};
+
+export default Login;
