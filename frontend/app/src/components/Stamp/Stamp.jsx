3241f727048905746ffee231acf753a6b4b2eaae
imamiya-masaki <imachan567@gmail.com>
2020-09-27 10:19:16 +0900

    wip

--- frontend/app/src/components/Stamp/Stamp.jsx
+++ frontend/app/src/components/Stamp/Stamp.jsx
@@ -1,12 +1,13 @@
-import React from "react";
+import React, { useState } from "react";
 import styles from "./Stamp.module.css";
 import Navbar from "../Navbar/Navbar";
-
+import stampsheet from "../../images/stampsheet.png";
 const Stamp = (props) => {
+    var [stamp, SetStamp] = useState("")
     return (
         <div className={styles.container}>
             <Navbar />
-            Stamp
+            <img src={stampsheet}/>
         </div>
     );
 };
