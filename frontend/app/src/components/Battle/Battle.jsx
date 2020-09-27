6ec5cc30d874659805825863db778ee3d58d71d3
imamiya-masaki <imachan567@gmail.com>
2020-09-27 13:59:04 +0900

    ばとるbuttonとか

--- frontend/app/src/components/Battle/Battle.jsx
+++ frontend/app/src/components/Battle/Battle.jsx
@@ -11,13 +11,46 @@ import axios from "axios";
 
 
 const Battle = (props) => {
+    var [flag, SetFlag] = useState(false)
     
-  
-    return (
-        <div className={styles.container}>
+    var onclickEvent = function () {
+        console.log('true!!', flag)
+        SetFlag(true)
+    }
+    var doDom = function () {
+        if (flag) {
+            return (<div class="ui modal">
+            <div class="header">Header</div>
+            <div class="image content">
+              <img class="image"/>
+              <div class="description">
+                <p></p>
+              </div>
+            </div>
+          </div>);
+        }
+    }
+    var doDomStamps = function () {
+        if (!false) {
+            return (<img src={SomeStampkun} className={styles.somestamp} alt="複数スタンプ" />);
+        }　else {
+            return (<div></div>);
+        }
+    }
+    var doDomVic = function () {
+        if (flag) {
+            return (<div>勝利!!</div>);
+        }　else {
+            return (<div></div>);
+        }
+    }
+    var getDom = function () {
+        return (<div className={styles.container}>
             <Navbar />
             <div className="ui center aligned grid">
-                <img src={SomeStampkun} className={styles.somestamp} alt="複数スタンプ" />
+                {doDom}
+                {doDomStamps}
+                <img src={SomeStampkun} className={styles.somestamp}  alt="複数スタンプ" />
                 <img src={Hissyoukun} className={styles.hissyoukun} alt="必勝くん" />
                 <img src={Vs} className={styles.vs} alt="VSマーク" />
 
@@ -29,6 +62,30 @@ const Battle = (props) => {
                     </ul>
                 </div>
             </div>
+            {doDomVic}
+            <button onClick={onclickEvent} className="ui button">バトル！！</button>
+        </div>);
+    }
+    return (
+        <div className={styles.container}>
+            <Navbar />
+            <div className="ui center aligned grid">
+                {doDom}
+                {doDomStamps}
+                <img src={SomeStampkun} className={styles.somestamp}  alt="複数スタンプ" />
+                <img src={Hissyoukun} className={styles.hissyoukun} alt="必勝くん" />
+                <img src={Vs} className={styles.vs} alt="VSマーク" />
+
+                <div className={styles.status}>
+                    <ul>
+                        <li>攻撃（勉強量）：？？？</li>
+                        <li>防御（ログイン回数）：？？？</li>
+                        <li>HP（進研模試偏差値）：？？？</li>
+                    </ul>
+                </div>
+            </div>
+            {doDomVic}
+            <button onClick={onclickEvent} className="ui button">バトル！！</button>
         </div>
     );
 };
