fd33fc42d7c7a80e567f2972123db46104259d35
Tanaka Kent <tanaken0304.0123@gmail.com>
2020-09-27 09:57:25 +0900

    html profile

--- frontend/app/src/components/Battle/Battle.jsx
+++ frontend/app/src/components/Battle/Battle.jsx
@@ -1,12 +1,50 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import posed from 'react-pose';
 import styles from "./Battle.module.css";
 import Navbar from "../Navbar/Navbar";
+//import Stampkun from "../../images/stamp_kun.png";
+import SomeStampkun from "../../images/somestamp.png";
+import Hissyoukun from "../../images/hissyou_kun.png";
+import Vs from "../../images/vs.png";
+import axios from "axios";
+
+const props = {
+    visible: { 
+      opacity: 1,
+      x: 0
+    },
+    hidden: { 
+      opacity: 0,
+      x: '100%'
+    }
+};
 
 const Battle = (props) => {
+    
+    const [visible, setVisible] = useState(false);
+
+    useEffect(() => {
+        setVisible(true)
+    }, []);
+
+    const MovedStamp = posed.div({});
+
     return (
         <div className={styles.container}>
             <Navbar />
-            Battle
+            <div className="ui center aligned grid">
+                <img src={SomeStampkun} className={styles.somestamp} alt="複数スタンプ" />
+                <img src={Hissyoukun} className={styles.hissyoukun} alt="必勝くん" />
+                <img src={Vs} className={styles.vs} alt="VSマーク" />
+
+                <div className={styles.status}>
+                    <ul>
+                        <li>攻撃（勉強量）：？？？</li>
+                        <li>防御（ログイン回数）：？？？</li>
+                        <li>HP（進研模試偏差値）：？？？</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     );
 };
